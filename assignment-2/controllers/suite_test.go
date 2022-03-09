81218380199a118d96d819b7d26991b5d4d45978
Nitin M <nitinm@Velotios-Air.bbrouter>
2022-03-09 11:04:45 +0530

    assignment-3

--- assignment-2/controllers/suite_test.go
+++ assignment-2/controllers/suite_test.go
@@ -20,6 +20,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
